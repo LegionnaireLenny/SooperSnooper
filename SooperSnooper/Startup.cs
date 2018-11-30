78852c64148e0c15e7d075cc287d69a4525cc41f
Lenny Legionnaire <legionnairelenny@tutanota.com>
2018-11-30 13:43:59 -0600

    Add project files.

---
+++ SooperSnooper/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SooperSnooper.Startup))]
+namespace SooperSnooper
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
